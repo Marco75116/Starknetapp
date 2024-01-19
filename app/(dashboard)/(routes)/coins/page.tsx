6542a23bfc446b033499e2800e499b46e0dff024
marc-antoine ourradour <ourradourm@gmail.com>
2024-01-19 18:52:29 +0100

    add accordeon for create form

--- app/(dashboard)/(routes)/coins/page.tsx
+++ app/(dashboard)/(routes)/coins/page.tsx
@@ -1,6 +1,14 @@
 import HeaderPage from "@/components/componentsPages/HeaderPage";
 import AccountForm from "@/components/form-account";
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
 import React from "react";
 
 const CoinPage = () => {
@@ -8,7 +16,23 @@ const CoinPage = () => {
     <div>
       <HeaderPage title="Coin" desc="Configure & Launch your cryptocurrency." />
       <div className="lg:w-[50%]">
-        <AccountForm />
+        <Card>
+          <CardContent className="pb-0">
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1" className="border-b-0 ">
+                <AccordionTrigger>
+                  <span className="flex flex-row gap-2">
+                    Create <Plus />
+                  </span>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <AccountForm />
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </CardContent>
+        </Card>
+        <Separator className="my-2" />
       </div>
     </div>
   );
